452e4c78c15a32e96c1580b7c13c428d3813f2e9
sintaro <dorichan0629@gmail.com>
2020-04-30 16:20:51 +0900

    modified  typo

--- stacks/cdk/ecr_stack.py
+++ stacks/cdk/ecr_stack.py
@@ -1,13 +1,10 @@
-from aws_cdk import (core, aws_ecr as ecr)
+from aws_cdk import core, aws_ecr as ecr
 
 
 class ECRStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        
-        ecr_repository = ecr.Repository(
-            self,
-            id='CdkFargateFastAPI',
-            repository_name='cdk-fargate-fastapi'
+
+        ecr.Repository(
+            self, id="CDKFargateFastAPI", repository_name="cdk-fargate-fastapi"
         )
